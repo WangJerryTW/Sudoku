2ea186da5bbccf6a6653fa5541fae9df6ca1c0be
WangJerryTW <41264147+WangJerryTW@users.noreply.github.com>
2018-09-03 23:04:18 +0800

    add Mainpage and BattlePage
    加兩個頁面進行互動

--- Sudoku/Sudoku/MainPage.xaml.cs
+++ Sudoku/Sudoku/MainPage.xaml.cs
@@ -13,5 +13,11 @@ namespace Sudoku
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void ChallangeMode_Clicked(object sender, EventArgs e)
+        {
+            //--頁面切換到數獨頁面
+            Application.Current.MainPage = new SudokuBattle();
+        }
+    }
 }
---
+++ Sudoku/Sudoku/SudokuBattle.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sudoku
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SudokuBattle : ContentPage
+	{
+		public SudokuBattle ()
+		{
+			InitializeComponent ();
+		}
+        private void SaveReturn_Clicked(object sender, EventArgs e)
+        {
+            //---儲存進度切回主頁面
+            Application.Current.MainPage = new MainPage();
+        }
+    }
+}
