af5e0b54575cf52a7c39e68bab48555c7d4738ff
eol3 <imagine90359@yahoo.com.tw>
2018-09-12 02:26:33 +0000

    upgrade to vue cli 3 Ewwwwwwwww

---
+++ webapp/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #354242;
+}
+#app {
+  color: #fff;
+  margin: auto;
+  width: 40%;
+}
+</style>
---
+++ webapp/src/views/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1 style="text-align:center">數獨遊戲</h1>
+    <ul>
+        <li v-for="item in sudo">
+            <router-link :to="'sudoku/'+item.id">{{ item.id }}</router-link>
+            <span v-if="item.tag==1">very easy</span>
+            <span v-else-if="item.tag==2">easy</span>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      sudo:[]
+    }
+  },
+  created () {
+    this.axios.get('http://api.pureday.life/sudo/')
+      .then(response => (this.sudo = response.data))
+  }
+}
+</script>
+
+<style>
+  li{
+    padding:5px;
+    font-size:22px;
+  }
+  a{
+    color:white;
+  }
+</style>
